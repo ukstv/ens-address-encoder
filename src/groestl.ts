0fe0ecda80482e93c49d6c56bddd1faad8dcc740
Sergey Ukustov <sergey@ukstv.me>
2023-09-03 10:54:15 +0300

    b64a

--- src/groestl.ts
+++ src/groestl.ts
@@ -158,37 +158,36 @@ const NJ64 = [
 const R64 = [0n, 1n, 2n, 3n, 4n, 5n, 6n, 7n, 8n, 9n, 10n, 11n, 12n, 13n];
 
 function compress(int64buf: Array<bigint>, state: any) {
-  var g = new Array(16);
-  var m = new Array<bigint>(16);
+  let g = new Array<bigint>(16);
+  let m = new Array<bigint>(16);
   for (let uu = 0; uu < 16; uu++) {
     m[uu] = int64buf[uu];
-    g[uu] = bigintToU64(m[uu] ^ state[uu].bigint);
+    g[uu] = m[uu] ^ state[uu].bigint;
   }
-  var t = new Array(16);
+  let t = new Array(16);
   for (let r = 0; r < 14; r++) {
     for (var i = 0; i < 16; i++) {
       let b = J64[i] + R64[r];
-      const gN = g[i].bigint;
-      g[i] = bigintToU64(xor64(gN, b << 56n));
+      g[i] = xor64(g[i], b << 56n);
     }
 
     for (let uu = 0; uu < 16; uu++) {
       /* tslint:disable:no-bitwise */
       t[uu] = bigintToU64(
         xor64(
-          T0[B64(0, g[uu])],
-          T1[B64(1, g[(uu + 1) & 0xf])],
-          T2[B64(2, g[(uu + 2) & 0xf])],
-          T3[B64(3, g[(uu + 3) & 0xf])],
-          T4[B64(4, g[(uu + 4) & 0xf])],
-          T5[B64(5, g[(uu + 5) & 0xf])],
-          T6[B64(6, g[(uu + 6) & 0xf])],
-          T7[B64(7, g[(uu + 11) & 0xf])],
+          T0[B64A(0, g[uu])],
+          T1[B64A(1, g[(uu + 1) & 0xf])],
+          T2[B64A(2, g[(uu + 2) & 0xf])],
+          T3[B64A(3, g[(uu + 3) & 0xf])],
+          T4[B64A(4, g[(uu + 4) & 0xf])],
+          T5[B64A(5, g[(uu + 5) & 0xf])],
+          T6[B64A(6, g[(uu + 6) & 0xf])],
+          T7[B64A(7, g[(uu + 11) & 0xf])],
         ),
       );
     }
-    let temp = g;
-    g = t;
+    let temp = g.map((r) => bigintToU64(r));
+    g = t.map((u) => u.bigint);
     t = temp;
   }
   for (let r = 0; r < 14; r++) {
@@ -215,7 +214,7 @@ function compress(int64buf: Array<bigint>, state: any) {
     t = temp;
   }
   for (let uu = 0; uu < 16; uu++) {
-    state[uu] = bigintToU64(xor64(state[uu].bigint, g[uu].bigint, m[uu]));
+    state[uu] = bigintToU64(xor64(state[uu].bigint, g[uu], m[uu]));
   }
 }
 
