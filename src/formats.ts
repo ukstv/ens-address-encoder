153e08fd304dd4445a422d5dbf4996cdc694ebb2
Sergey Ukustov <sergey@ukstv.me>
2023-09-03 13:04:42 +0300

    aargh nice grs

--- src/formats.ts
+++ src/formats.ts
@@ -4,17 +4,18 @@ import { hexToBytes } from "@noble/hashes/utils";
 import { fromCoder } from "./format.js";
 import { makeGroestlCoder } from "./groestl.js";
 
-const h = hexToBytes;
+const h = (...hexes: Array<string>) => hexes.map(hexToBytes);
+const c = fromCoder;
 export const FORMATS: Array<IFormat> = [
-  fromCoder("BTC", 0, makeBitcoinCoder("bc", [h("00")], [h("05")])),
-  fromCoder("LTC", 2, makeBitcoinCoder("ltc", [h("30")], [h("32"), h("05")])),
-  fromCoder("DOGE", 3, makeBitcoinBase58Check([h("1e")], [h("16")])),
-  fromCoder("RDD", 4, makeBitcoinBase58Check([h("3d")], [h("05")])),
-  fromCoder("DASH", 5, makeBitcoinBase58Check([h("4c")], [h("10")])),
-  fromCoder("PPC", 6, makeBitcoinBase58Check([h("37")], [h("75")])),
-  fromCoder("NMC", 7, BS58),
-  fromCoder("VIA", 14, makeBitcoinBase58Check([h("47")], [h("21")])),
-  fromCoder("GRS", 17, makeGroestlCoder("grs", [h("24")], [h("05")])),
+  c("BTC", 0, makeBitcoinCoder("bc", h("00"), h("05"))),
+  c("LTC", 2, makeBitcoinCoder("ltc", h("30"), h("32", "05"))),
+  c("DOGE", 3, makeBitcoinBase58Check(h("1e"), h("16"))),
+  c("RDD", 4, makeBitcoinBase58Check(h("3d"), h("05"))),
+  c("DASH", 5, makeBitcoinBase58Check(h("4c"), h("10"))),
+  c("PPC", 6, makeBitcoinBase58Check(h("37"), h("75"))),
+  c("NMC", 7, BS58),
+  c("VIA", 14, makeBitcoinBase58Check(h("47"), h("21"))),
+  c("GRS", 17, makeGroestlCoder("grs", h("24"), h("05"))),
 ];
 
 export const formatsByName: Record<string, IFormat> = Object.fromEntries(
